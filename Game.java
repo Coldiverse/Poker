bcfe0fb277d26077098bf65616dfb84947c982a2
Ben Wilke <wilkeb8@gmail.com>
2018-04-26 22:18:11 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -28,12 +28,11 @@ public class Game extends Canvas implements Runnable {
 		
 		r = new Random();
 		
-		handler.addObject(new Player(WIDTH/2-32, HEIGHT/2-32, ID.Player));
-		
-		for(int i = 0; i < 1; i++) {
-			handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy));
-		}
-		
+		handler.addObject(new Player(WIDTH/2-32, HEIGHT/2-32, ID.Player, handler));
+		handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
+		handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
+		handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
+		handler.addObject(new BasicEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.BasicEnemy, handler));
 	}
 	
 	public synchronized void start() {
