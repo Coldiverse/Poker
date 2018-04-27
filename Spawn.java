cd1a7804cfb96484b4924abd012463e3cdf6a428
Ben Wilke <wilkeb8@gmail.com>
2018-04-27 16:45:21 -0400

    Create Spawn.java

---
+++ Spawn.java
@@ -0,0 +1,19 @@
+public class Spawn {
+
+    private Handler handler;
+    private HUD hud;
+    
+    public Spawn(Handler handler, HUD hud) {
+        this.handler = handler;
+        this.hud = hud;
+    }
+
+    public void tick() {
+
+    }
+   
+    public void render() {
+
+    }
+
+}
