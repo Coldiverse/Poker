fa519ad48342f5e8bc5be50f6a6cf606d14fb763
Ben Wilke <wilkeb8@gmail.com>
2018-04-26 22:19:44 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.Random;
 
 public class Player extends GameObject {
 
 	Random r = new Random();
+	Handler handler;
 	
-	public Player(int x, int y, ID id) {
+	public Player(int x, int y, ID id, Handler handler) {
 		super(x, y, id);
+		this.handler = handler;
 		
 		
 	}
@@ -23,10 +26,24 @@ public class Player extends GameObject {
 	
 		x = Game.clamp(x, 0, Game.WIDTH - 38);
 		y = Game.clamp(y, 0, Game.HEIGHT - 61);
+		
+		collision();
+	}
+	
+	private void collision() {
+		for(int i = 0; i < handler.object.size(); i++) {
+			
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getId() == ID.BasicEnemy) {
+				if(getBounds().intersects(tempObject.getBounds()))
+					HUD.HEALTH -= 2;
+			}
+		}
 	}
 
 	public void render(Graphics g) {
-		if(id == ID.Player) g.setColor(Color.white);
+		g.setColor(Color.white);
 		g.fillRect(x, y, 32, 32);
 		
 	}
