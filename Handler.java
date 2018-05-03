2ad24f3582f669bb949f8e813785a2f23607740e
Ben Wilke <wilkeb8@gmail.com>
2018-05-03 07:50:05 -0500

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -9,7 +9,7 @@ public class Handler {
 		for(int i = 0; i < object.size();i++) {
 			GameObject tempObject = object.get(i);
 			
-			tempObject.tick();			
+			tempObject.tick();
 		}
 	}
 	
@@ -20,6 +20,16 @@ public class Handler {
 			tempObject.render(g);
 		}
 		
+	}
+	
+	public void clearEnemys() {
+		for(int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+			
+			if(tempObject.getId() == ID.Player)
+				object.clear();
+				addObject(new Player((int)tempObject.getX(), (int)tempObject.getY(), ID.Player, this));
+		}
 	}
 	
 	public void addObject(GameObject object) {
