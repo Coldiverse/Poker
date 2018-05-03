8f2798745732bf6f443d27cc4964fc78610fe733
Ben Wilke <wilkeb8@gmail.com>
2018-05-03 07:48:08 -0500

    Update Spawn.java

--- Spawn.java
+++ Spawn.java
@@ -1,19 +1,54 @@
+import java.util.Random;
+
 public class Spawn {
 
-    private Handler handler;
-    private HUD hud;
-    
-    public Spawn(Handler handler, HUD hud) {
-        this.handler = handler;
-        this.hud = hud;
-    }
+	private Handler handler;
+	private HUD hud;
+	private Random r = new Random();
+	
+	private int scoreKeep = 0;	
+	
+	public Spawn(Handler handler, HUD hud) {
+		this.handler = handler;
+		this.hud = hud;
+	}
+	
+	public void tick() {
+		scoreKeep++;
+		
+		if(scoreKeep >= (300)) {
+			scoreKeep = 0;
+			hud.setLevel(hud.getLevel() + 1);
+			if(hud.getLevel() == 1) {
+				handler.addObject(new BasicEnemy(Game.WIDTH-50, Game.HEIGHT + 50, ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 2) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 3) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 4) {
+				handler.addObject(new FastEnemyV(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemyV, handler));
+			}else if(hud.getLevel() == 5) {
+				handler.addObject(new FastEnemyH(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemyH, handler));
+			}else if(hud.getLevel() == 6) {
+				handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
+			}else if(hud.getLevel() == 7) {
+				handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));			
+			}else if(hud.getLevel() == 8) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 9) {
+				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+			}else if(hud.getLevel() == 10) {
+				handler.clearEnemys();
+				handler.addObject(new Boss1(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.Boss1, handler));
+			}else if(hud.getLevel() == 11) {
+				handler.addObject(new FastEnemyH(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemyH, handler));
+			}else if(hud.getLevel() == 12) {
+				handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
+			}else if(hud.getLevel() == 13) {
+				handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));			
+			}
 
-    public void tick() {
-
-    }
-   
-    public void render() {
-
-    }
-
+				
+		}
+	}
 }
