8262920de252975033f970f2f5f34efcfa2c359d
Ben Wilke <wilkeb8@gmail.com>
2018-05-03 07:49:48 -0500

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -4,27 +4,47 @@ import java.awt.Graphics;
 public class HUD {
 
 	
-	public static int HEALTH = 100;
-	private int score = 0;
-	private int level = 1;
+	public static float HEALTH = 100;
+	private float greenValue = 255f;
 	
-	private int greenValue = 255;
+	private float score = 0;
+	private float level = 1;
 	
 	public void tick() {
-		
 		HEALTH = Game.clamp(HEALTH, 0, 100);
 		greenValue = Game.clamp(greenValue, 0, 255);
+		
 		greenValue = HEALTH * 2;
+		
+		score++;
 	}
 	
 	public void render(Graphics g) {
 		g.setColor(Color.gray);
 		g.fillRect(15, 15, 200, 32);
-		g.setColor(new Color(75, greenValue, 0));
-		g.fillRect(15, 15, HEALTH * 2, 32);		
+		g.setColor(new Color(75, (int)greenValue, 0));
+		g.fillRect(15, 15, (int) (HEALTH * 2), 32);		
 		g.setColor(Color.white);
 		g.drawRect(15, 15, 200, 32);
-		g.drawString("Score: " + score, 10, 48);
-		g.drawString("Level: " + level, 10, 64);
+		
+		g.drawString("Score: " + score, 15, 64);
+		g.drawString("Level: " + level, 15, 80);
+}
+	
+	private void score(float score) {
+		this.score = score;
+	}
+	
+
+	private float getScore() {
+		return score;
+	}
+	
+	public float getLevel() {
+		return level;
+	}
+	
+	public void setLevel(float level) {
+		this.level = level;
 	}
 }
