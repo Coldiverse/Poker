2746be3baa6d269659b0a2ba5904f2e13fec1c8c
Ben Wilke <wilkeb8@gmail.com>
2018-05-04 07:34:47 -0500

    Update BasicEnemy.java

--- BasicEnemy.java
+++ BasicEnemy.java
@@ -1,38 +1,42 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.Random;
 
 public class BasicEnemy extends GameObject{
 	
 	private Handler handler;
 
-	public BasicEnemy(int x, int y, ID id, Handler handler) {
-		super(x, y, id);
-		
-		this.handler = handler;
 
+
+	public BasicEnemy(float x, float y, ID id, Handler handler) {
+		super(x, y, id);
+
+		this.handler = handler;
+		
 		velX = 5;
 		velY = 5;
 	}
 
 	public Rectangle getBounds() {
-		return new Rectangle(x, y, 16, 16);
+		return new Rectangle((int)x, (int)y, 16, 16);
 	}
 
 	public void tick() {
 		x += velX;
 		y += velY;
 		
+		
 		if(y <= 0 || y >= Game.HEIGHT - 40) velY *= -1;
 		if(x <= 0 || x >= Game.WIDTH - 16) velX *= -1;
 		
-		handler.addObject(new Trail(x, y, ID.Trail, Color.red, 16, 16, 0.05f, handler));
+		handler.addObject(new Trail((int)x, (int)y, ID.Trail, Color.red, 16, 16, 0.05f, handler));
 	}
 
 
 	public void render(Graphics g) {
 		g.setColor(Color.red);
-		g.fillRect(x, y, 16, 16);
+		g.fillRect((int)x, (int)y, 16, 16);
 		
 	}
 
